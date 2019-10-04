2b1aa7edf175048f0dad53465cd94def22267a15
nethernet <eryk.dworak@gmail.com>
2019-10-04 21:35:05 +0100

    converting db connection string to env variable

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 application = Flask(__name__)
 
-application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{DB_USERNAME}:{DB_PASSWORD}@localhost/user_db'
+application.config['SQLALCHEMY_DATABASE_URI'] = environ.get("SQLALCHEMY_DATABASE_URI")
 application.config['SQLALCHEMY_ECHO'] = True
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
