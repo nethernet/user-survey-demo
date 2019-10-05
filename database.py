23352cf00e642ffc13287d8ae8d4416e4be66889
nethernet <eryk.dworak@gmail.com>
2019-10-05 01:12:12 +0100

    moving env variable to dotenv

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import environ
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(os.getcwd(), '.env'))
 
 application = Flask(__name__)
 
-application.config['SQLALCHEMY_DATABASE_URI'] = environ.get("SQLALCHEMY_DATABASE_URI")
+application.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 application.config['SQLALCHEMY_ECHO'] = True
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
